3cf64fa8dba3bf6ac6a52a27713faedc769dc0e1
Peter Marangos <lifelessmango@gmail.com>
2018-04-25 21:27:02 -0400

    train_model.py: Use INPUT_FILE variable

--- email/train_model.py
+++ email/train_model.py
@@ -1,7 +1,8 @@
 from textgenrnn import textgenrnn
 
+INPUT_FILE = 'mail.txt'
 MODEL_NAME = 'peter_emails'
 
 model = textgenrnn()
-model.train_from_file('mail.txt', num_epochs=25, batch_size=100)
+model.train_from_file(INPUT_FILE, num_epochs=25, batch_size=100)
 model.save(MODEL_NAME + '.hdf5')
