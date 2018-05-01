4ec905bfc769afc411d78cbdaa789d92833fb5d2
Peter Marangos <lifelessmango@gmail.com>
2018-04-30 21:27:48 -0400

    Add create_seq2seq_files.py

---
+++ email/create_seq2seq_files.py
@@ -0,0 +1,67 @@
+import pickle
+
+mail = pickle.load(open('messages.pickle', 'rb'))
+
+mail = sorted(mail, key=lambda x: x[2])
+
+in_messages = []
+out_messages = []
+vocab = set()
+
+for message in mail:
+    message_text = message[1]
+
+    # Replace Return Characters with <ret>
+    message_text = message_text.replace(b'\r\n', b'<ret>')
+    message_text = message_text.replace(b'\n', b'<ret>')
+
+    # Check for incoming message
+    reply_start = message_text.find(b'<ret>>')
+    if reply_start == -1:
+        continue
+
+    # Cut off more than one reply
+    second_reply_start = message_text.find(b'<ret>>>')
+    if second_reply_start != -1:
+        message_text = message_text[:second_reply_start]
+    
+    incoming = message_text[reply_start:].replace(b'<ret>>', b'<ret>')
+    response = message_text[:reply_start]
+
+    # Pad returns to count as separate words
+    incoming = incoming.replace(b'<ret>', b' <ret> ')
+    response = response.replace(b'<ret>', b' <ret> ')
+
+    # Remove double spaces
+    while incoming.find(b'  ') != -1 or response.find(b'  ') != -1:
+        incoming = incoming.replace(b'  ', b' ')
+        response = response.replace(b'  ', b' ')
+    incoming = incoming.strip()
+    response = response.strip()
+
+    # Remove punctuation
+    punctuation = b'.,()!?/\\:\'"*[]@#$%^&-+_=;'
+    incoming = ''.join([chr(ch) for ch in incoming if ch not in punctuation])
+    response = ''.join([chr(ch) for ch in response if ch not in punctuation])
+
+    print(incoming)
+    print('-'*50)
+    print(response)
+    print('-'*200)
+
+    in_messages.append(incoming)
+    out_messages.append(response)
+
+    vocab |= set(incoming.split(' '))
+    vocab |= set(response.split(' '))
+
+
+vocab_file = open('vocab', 'wb')
+vocab_text = '\n'.join(list(vocab))
+vocab_file.write(vocab_text.encode())
+
+in_file = open('email.in', 'wb')
+in_file.write('\n'.join(in_messages).encode())
+
+out_file = open('email.out', 'wb')
+out_file.write('\n'.join(out_messages).encode())
